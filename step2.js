6e6ea28cf97bd93ed8ac8a564f6193fbcfab71f2
Amita <amita.parikh@gmail.com>
2019-03-11 21:38:38 +0000

    My Step 2 buffering string

---
+++ step2.js
@@ -0,0 +1,39 @@
+var https = require('https');
+
+function getAndPrintHTML () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+  // / notice that https.get takes a callback with one parameter -
+  // response, which is a Stream that represents the HTTP response
+https.get(requestOptions, function (response, body) {
+  // set encoding of received data to UTF-8
+  response.setEncoding('utf8');
+
+  var buffer = "";
+
+  // the callback is invoked when a `data` chunk is received
+  response.on('data', function (data) {
+    console.log('Chunk Received. Length:', data.length);
+    buffer += data
+    // concatenate string
+  });
+
+  // the callback is invoked when all of the data has been received
+  // (the `end` of the stream)
+  response.on('end', function() {
+    console.log('Response stream complete.');
+    console.log(buffer);
+    // remember to console.log the buffer
+  });
+
+});
+
+}
+getAndPrintHTML(https);
+
+
+
+  /* Add your code here */
