65f8b4b935191062b468cdb54ed4983509f057a9
Amita <amita.parikh@gmail.com>
2019-03-11 21:27:16 +0000

    My Step !

--- step1.js
+++ step1.js
@@ -8,13 +8,14 @@ function getAndPrintHTMLChunks () {
   };
   // / notice that https.get takes a callback with one parameter -
   // response, which is a Stream that represents the HTTP response
-https.get(requestOptions, function (response) {
+https.get(requestOptions, function (response, body) {
   // set encoding of received data to UTF-8
   response.setEncoding('utf8');
 
   // the callback is invoked when a `data` chunk is received
   response.on('data', function (data) {
     console.log('Chunk Received. Length:', data.length);
+    console.log(data + '\n');
   });
 
   // the callback is invoked when all of the data has been received
